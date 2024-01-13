60d6d47794b4b46274f5443413cc364e4b10e90c
FarhanDewani <aryadidewani@gmail.com>
2024-01-13 17:52:33 +0700

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
